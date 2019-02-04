1ffc24c387ab2e66e58ad7bb95d72e5ab102a20f
anwesht <anwesh.tuladhar@gmail.com>
2019-02-04 16:03:47 -0500

    Starter code for project1.

---
+++ kern/pmm/MATOp/MATOp.c
@@ -0,0 +1,40 @@
+#include <lib/debug.h>
+#include "import.h"
+
+/** TASK 1:
+  * Allocation of a physical page.
+  *
+  * 1. - First, implement a naive page allocator that scans the allocation table (AT) 
+  *      using the functions defined in import.h to find the first unallocated page
+  *      with usable permission.
+  *    
+  *    Hint 1: (Q: Do you have to scan allocation table from index 0? Recall how you have
+  *    			initialized the table in pmem_init.)
+  *    
+  *    - Then mark the page as allocated in the allocation table and return the page
+  *    	 index of the page found. In the case when there is no available page found,
+  *      return 0.
+  * 2. Optimize the code with the memorization techniques so that you do not have to
+  *    scan the allocation table from scratch every time.
+  */
+unsigned int
+palloc()
+{
+  // TODO
+  return 0;
+} 
+
+
+/** TASK 2:
+  * Free of a physical page.
+  *
+  * This function marks the page with given index as unallocated in the allocation table.
+  *
+  * Hint: Simple. You have already implemented the functions required to check if a page
+  * 	  is allocated and to set the allocation status of a page index. (See import.h)
+  */
+void
+pfree(unsigned int pfree_index)
+{
+  // TODO
+}
