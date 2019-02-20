e2f5c08826ef0ffad1f25c9a1f6fea15139cef58
Daniel Sawyer <sawyermade0@gmail.com>
2019-02-19 20:29:56 -0500

    finished pmm

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-bash ./build.sh
-qemu-system-x86_64 -smp 1 -hda project0.img -serial mon:stdio -gdb tcp::4444 -m 512 -k en-us
-rm project0.img
+##Project0
+#bash ./build.sh
+#qemu-system-x86_64 -smp 1 -hda project0.img -serial mon:stdio -gdb tcp::4444 -m 512 -k en-us
+#rm project0.img
+##Project1
+make clean
+make -j8 TEST=1
+
